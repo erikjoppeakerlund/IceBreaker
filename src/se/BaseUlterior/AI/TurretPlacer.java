3bc2e12a1e9b07e5f9d74a48286f17fbe6790674
JopsyTheJoker <joppeakerlund@gmail.com>
2016-07-24 00:34:48 +0200

    improve algoritm for turret placement

--- src/se/BaseUlterior/AI/TurretPlacer.java
+++ src/se/BaseUlterior/AI/TurretPlacer.java
@@ -20,21 +20,26 @@ public class TurretPlacer {
 
 		float[] points = groundPiece.getPoints();
 		Vector2 direction = new Vector2();
+		float beyond = 0;
 		for (int i = 0; i < points.length - 2; i += 2) {
 			float fullLength = (float) Math
 					.sqrt(Math.pow(points[i + 2] - points[i], 2) + Math.pow(points[i + 3] - points[i + 1], 2));
-			// float beyond = turretsDistance - fullLength % turretsDistance;
 			int nrOfTurretsThis = (int) (fullLength / turretsDistance);
-			direction.set(points[i + 2] - points[i], points[i + 3] - points[i + 1]);
-			direction.normalise();
-			for (int j = 0; j < nrOfTurretsThis; j++) {
-				float turretX = points[i] + (turretsDistance) * direction.x;
-				float turretY = points[i + 1] + (turretsDistance) * direction.y;
+			if (beyond < fullLength) {
+				direction.set(points[i + 2] - points[i], points[i + 3] - points[i + 1]);
+				direction.normalise();
+				for (int j = 0; j < nrOfTurretsThis || j == 0; j++) {
+					float turretX = points[i] + (turretsDistance * j + beyond) * direction.x;
+					float turretY = points[i + 1] + (turretsDistance * j + beyond) * direction.y;
 
-				AITurretVectorShotLauncher turret = new AITurretVectorShotLauncher(
-						new Circle(turretX, turretY, 140).getPoints(), direction.getPerpendicular());
-				IceBreaker.objsToAdd.add(turret);
+					AITurretVectorShotLauncher turretInLoop = new AITurretVectorShotLauncher(
+							new Circle(turretX, turretY, 140).getPoints(), direction.getPerpendicular());
+					IceBreaker.objsToAdd.add(turretInLoop);
 
+				}
+				beyond = turretsDistance - fullLength % turretsDistance;
+			} else {
+				beyond -= fullLength;
 			}
 		}
 	}
