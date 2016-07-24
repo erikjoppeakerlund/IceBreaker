8b4eb4e2d6d145448744ae2d501e04b450285fc9
Solver <robin.jernestrand@gmail.com>
2016-07-24 05:38:12 +0200

    new background and two city parallaxes

--- src/se/BaseUlterior/Game/GroundMap.java
+++ src/se/BaseUlterior/Game/GroundMap.java
@@ -20,57 +20,70 @@ public class GroundMap {
 	public GroundMap() {
 		levelPieces = new ArrayList<>();
 		try {
-			Image parallax1 = new Image("res/img/BackgroundStars.jpg");
-			float parallaxHeight = parallax1.getHeight();
-			float parallaxWidth = parallax1.getWidth();
-			float[] edges = { 0, 0, 0, parallaxHeight, parallaxWidth, parallaxHeight, parallaxWidth, 0 };
-			Parallax parallax = new Parallax(edges, parallax1, 0.2f);
-			levelPieces.add(parallax);
-			IceBreaker.parallaxList.add(parallax);
+			Image BackgroundStars = new Image("res/img/Stars.png");
+			float backgrounHeight = BackgroundStars.getHeight();
+			float backgrounWidth = BackgroundStars.getWidth();
+			float[] edges = { 0, 0, 0, backgrounHeight, backgrounWidth, backgrounHeight, backgrounWidth, 0 };
+			Parallax parallaxStars = new Parallax(edges, BackgroundStars, 0.03f);
+			levelPieces.add(parallaxStars);
+			IceBreaker.parallaxList.add(parallaxStars);
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
-		// ______________________________________________________
+
+		try {
+			Image city = new Image("res/img/CityFar.png");
+			float backgrounHeight = city.getHeight();
+			float backgrounWidth = city.getWidth();
+			float[] edges = { 0, 0, 0, backgrounHeight, backgrounWidth, backgrounHeight, backgrounWidth, 0 };
+			Parallax parallaxStars = new Parallax(edges, city, 0.4f);
+			levelPieces.add(parallaxStars);
+			IceBreaker.parallaxList.add(parallaxStars);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Image city = new Image("res/img/City.png");
+			float backgrounHeight = city.getHeight();
+			float backgrounWidth = city.getWidth();
+			float[] edges = { 0, 0, 0, backgrounHeight, backgrounWidth, backgrounHeight, backgrounWidth, 0 };
+			Parallax parallaxStars = new Parallax(edges, city, 0.5f);
+			levelPieces.add(parallaxStars);
+			IceBreaker.parallaxList.add(parallaxStars);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 
 		float thickness = 150.0f;
 
-		float[] wallLeftPoints = new float[] { -thickness, -thickness, -thickness,
-				Constants.CANVAS_HEIGHT_FULL + thickness, 0, Constants.CANVAS_HEIGHT_FULL + thickness, 0, -thickness };
+		float[] wallLeftPoints = new float[] { -thickness, -thickness, -thickness, Constants.CANVAS_HEIGHT_FULL + thickness, 0, Constants.CANVAS_HEIGHT_FULL + thickness, 0, -thickness };
 
 		GameObject wallLeft = new WorldBuilderGroundSolid(wallLeftPoints);
 		levelPieces.add(wallLeft);
 
-		float[] wallRightPoints = new float[] { Constants.CANVAS_WIDTH_FULL, -thickness, Constants.CANVAS_WIDTH_FULL,
-				Constants.CANVAS_HEIGHT_FULL + thickness, Constants.CANVAS_WIDTH_FULL + thickness,
-				Constants.CANVAS_HEIGHT_FULL + thickness, Constants.CANVAS_WIDTH_FULL + thickness, -thickness };
+		float[] wallRightPoints = new float[] { Constants.CANVAS_WIDTH_FULL, -thickness, Constants.CANVAS_WIDTH_FULL, Constants.CANVAS_HEIGHT_FULL + thickness, Constants.CANVAS_WIDTH_FULL + thickness, Constants.CANVAS_HEIGHT_FULL + thickness, Constants.CANVAS_WIDTH_FULL + thickness, -thickness };
 
 		GameObject wallRight = new WorldBuilderGroundSolid(wallRightPoints);
 		levelPieces.add(wallRight);
 
-		float[] floorPoints = new float[] { -thickness, Constants.CANVAS_HEIGHT_FULL,
-				Constants.CANVAS_WIDTH_FULL + thickness, Constants.CANVAS_HEIGHT_FULL,
-				Constants.CANVAS_WIDTH_FULL + thickness, Constants.CANVAS_HEIGHT_FULL + thickness, -thickness,
-				Constants.CANVAS_HEIGHT_FULL + thickness, -thickness, Constants.CANVAS_HEIGHT_FULL };
+		float[] floorPoints = new float[] { -thickness, Constants.CANVAS_HEIGHT_FULL, Constants.CANVAS_WIDTH_FULL + thickness, Constants.CANVAS_HEIGHT_FULL, Constants.CANVAS_WIDTH_FULL + thickness, Constants.CANVAS_HEIGHT_FULL + thickness, -thickness, Constants.CANVAS_HEIGHT_FULL + thickness, -thickness, Constants.CANVAS_HEIGHT_FULL };
 
 		GameObject floor = new WorldBuilderGroundSolid(floorPoints);
 		levelPieces.add(floor);
 
-		float[] roofPoints = new float[] { -thickness, 0, Constants.CANVAS_WIDTH_FULL + thickness, 0,
-				Constants.CANVAS_WIDTH_FULL + thickness, -thickness, -thickness, -thickness };
+		float[] roofPoints = new float[] { -thickness, 0, Constants.CANVAS_WIDTH_FULL + thickness, 0, Constants.CANVAS_WIDTH_FULL + thickness, -thickness, -thickness, -thickness };
 
 		GameObject roof = new WorldBuilderGroundSolid(roofPoints);
 		levelPieces.add(roof);
 
-		float[] wholeScene = new float[] { 0.0f, 0.0f, 0.0f, Constants.CANVAS_HEIGHT_FULL, Constants.CANVAS_WIDTH_FULL,
-				Constants.CANVAS_HEIGHT_FULL, Constants.CANVAS_WIDTH_FULL, 0.0f };
+		float[] wholeScene = new float[] { 0.0f, 0.0f, 0.0f, Constants.CANVAS_HEIGHT_FULL, Constants.CANVAS_WIDTH_FULL, Constants.CANVAS_HEIGHT_FULL, Constants.CANVAS_WIDTH_FULL, 0.0f };
 
-		WorldBuilderForce generalGravity = new WorldBuilderForce(wholeScene, 0, Constants.GENERAL_GRAVITY,
-				Color.transparent);
+		WorldBuilderForce generalGravity = new WorldBuilderForce(wholeScene, 0, Constants.GENERAL_GRAVITY, Color.transparent);
 
 		levelPieces.add(generalGravity);
 
-		levelPieces.add(new WorldBuilderLiquid(new float[] { 0, 0, 0, Constants.CANVAS_HEIGHT, Constants.CANVAS_WIDTH,
-				Constants.CANVAS_HEIGHT, Constants.CANVAS_WIDTH, 0 }, Density.AIR, true));
+		levelPieces.add(new WorldBuilderLiquid(new float[] { 0, 0, 0, Constants.CANVAS_HEIGHT, Constants.CANVAS_WIDTH, Constants.CANVAS_HEIGHT, Constants.CANVAS_WIDTH, 0 }, Density.AIR, true));
 
 	}
 
